b66d948433639bdb5451e4228b2c696c430223bc
a.tarasov <taras52@ya.ru>
2018-01-05 18:00:39 +0100

    studies

--- Meals/Dishes.py
+++ Meals/Dishes.py
@@ -1,27 +1,28 @@
-meals = {}
-with open('dishes.txt') as f:
-    for line in f:
-        key_meal = line.strip().lower()
-        meals[key_meal] = []
-        meals_number = f.readline()
-        ing_number = int(meals_number)
-        i = 1
-        ing_list_total = []
-        while i <= ing_number:
-            ing_list = f.readline().strip()
-            ing_list = list(ing_list.split(' | '))
-            ing_list[1] = int(ing_list[1])
-            ing_dict = {'ingridient_name': ing_list[0], 'quantity': ing_list[1], 'measure': ing_list[2]}
-            ing_list_total.append(ing_dict)
-            i += 1
-        meals[key_meal] = ing_list_total
-        f.readline()
-print(meals)
-cook_book = meals
+
+def read_meals_file(a):
+    meals = {}
+    with open(a) as f:
+        for line in f:
+            key_meal = line.strip().lower()
+            meals_number = f.readline()
+            ing_number = int(meals_number)
+            i = 1
+            ing_list_total = []
+            while i <= ing_number:
+                ing_list = f.readline().strip()
+                ing_list = list(ing_list.split(' | '))
+                ing_list[1] = int(ing_list[1])
+                ing_dict = {'ingridient_name': ing_list[0], 'quantity': ing_list[1], 'measure': ing_list[2]}
+                ing_list_total.append(ing_dict)
+                i += 1
+            meals[key_meal] = ing_list_total
+            f.readline()
+    return meals
 
 
-def get_shop_list_by_dishes(a, b):
+def get_shop_list_by_dishes(a, b, c):
     shop_list = {}
+    cook_book = read_meals_file(c)
     for dish in a:
         for ing in cook_book[dish]:
             new_shop_list_item = dict(ing)
@@ -38,11 +39,11 @@ def print_shop_list(shop_list):
         print(f'{shop_list_item["ingridient_name"]} {shop_list_item["quantity"]} {shop_list_item["measure"]}')
 
 
-def create_shop_list():
+def create_shop_list(meals_list):
     person_count = int(input('Введите количество человек: '))
     dishes = input('Введите блюда в расчете на одного человека (через запятую): ').lower().split(', ')
-    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    shop_list = get_shop_list_by_dishes(dishes, person_count, meals_list)
     print_shop_list(shop_list)
 
 
-create_shop_list()
+create_shop_list('dishes.txt')
