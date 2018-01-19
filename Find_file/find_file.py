5db01a4dfba43a37072fa5b143fccf1e5b14a20e
a.tarasov <taras52@ya.ru>
2018-01-19 20:13:42 +0300

    studies

--- Find_file/find_file.py
+++ Find_file/find_file.py
@@ -7,11 +7,13 @@ if __name__ == '__main__':
     filelist = [f for f in os.listdir(os.path.join(current_dir, migrations)) if f.endswith('.sql')]
     while True:
         searching_data = input('Введите строку:')
+        aux_list = []
         for file in filelist:
             with open(os.path.join(current_dir, migrations, file)) as f:
                 data = f.read()
-                if searching_data in data is False:
-                    filelist.remove(file)
+                if searching_data in data:
+                    aux_list.append(file)
+        filelist = aux_list
         for file in filelist:
             print(file)
         print('Всего', len(filelist))
