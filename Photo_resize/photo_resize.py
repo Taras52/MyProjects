554d1593f71cb907e5972e8ed1a3fbea1ca60ab2
a.tarasov <taras52@ya.ru>
2018-01-23 00:03:08 +0300

    studies

--- Photo_resize/photo_resize.py
+++ Photo_resize/photo_resize.py
@@ -1,12 +1,22 @@
 import subprocess
 import os
 
+
+def image_resize(source):
+    filelist = [f for f in os.listdir(source)]
+    cp_path_output = os.path.join(current_dir, 'Result')
+    try:
+        os.makedirs(cp_path_output)
+    except FileExistsError:
+        pass
+    for file in filelist:
+        cp_path_input = os.path.join(current_dir, 'Source', file)
+        cp_path = 'cp' + ' ' + cp_path_input + ' ' + cp_path_output
+        sips_file = 'sips --resampleWidth 200' + ' ' + os.path.join(current_dir, 'Result', file)
+        subprocess.run(cp_path, shell=True)
+        subprocess.run(sips_file, shell=True)
+
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
-filelist = [f for f in os.listdir(os.path.join(current_dir, 'Source'))]
-cp_path_output = os.path.join(current_dir, 'Result')
-for file in filelist:
-    cp_path_input = os.path.join(current_dir, 'Source', file)
-    cp_path = 'cp' + ' ' + cp_path_input + ' ' + cp_path_output
-    sips_file = 'sips --resampleWidth 200' + ' ' + os.path.join(current_dir, 'Result', file)
-    subprocess.run(cp_path, shell=True)
-    subprocess.run(sips_file, shell=True)
+source_path = os.path.join(current_dir, 'Source')
+image_resize(source_path)
