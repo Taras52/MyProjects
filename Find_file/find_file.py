935b7a3d6634a3b9042ec7b4c978f8feabbd9502
a.tarasov <taras52@ya.ru>
2018-01-28 20:25:01 +0300

    studies

--- Find_file/find_file.py
+++ Find_file/find_file.py
@@ -3,13 +3,14 @@ import os
 migrations = 'Migrations'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 def search_data_in_file(input_data, search_list):
     aux_list = []
-    for file in search_list:
-        with open(os.path.join(current_dir, migrations, file)) as f:
+    for file_name in search_list:
+        with open(os.path.join(current_dir, migrations, file_name)) as f:
             data = f.read()
             if input_data in data:
-                aux_list.append(file)
+                aux_list.append(file_name)
     search_list = aux_list
     return search_list
 
